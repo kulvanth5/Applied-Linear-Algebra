19fb35742b30e429945f08ceb97f1c510e1ef70e
kulvanth5 <93081187+kulvanth5@users.noreply.github.com>
2023-08-05 15:16:56 +0530

    cofactor matrix of a square matrix

---
+++ cofactor.py
@@ -0,0 +1,89 @@
+import sys
+
+def matrix_create(n):
+
+	matrix = []
+
+	for i in range(0,n):
+
+		temp = []
+
+		for j in range(0,n):
+
+			print('enter',i,j,'th value')
+			temp.append(int(input()))
+
+		matrix.append(temp)
+
+	return matrix
+
+
+
+def subMatrix(r,c,m):
+
+	sm = []
+
+	for i in range(0,len(m)):
+	
+		temp = []
+	
+		if i != r:
+		
+			for j in range(0,len(m[0])):
+			
+				if j != c:
+					
+					temp.append(m[i][j])
+					
+			sm.append(temp)
+					
+	return sm
+	
+def det(m):
+
+	if len(m) == 1:
+		return m[0][0]
+
+	if len(m) == 2:
+		return m[0][0]*m[1][1] - m[0][1]*m[1][0]
+		
+	ans = 0
+	
+	for i in range(len(m)):
+	
+		sm = subMatrix(0,i,m)
+		ans = ans + pow(-1,0+i)*m[0][i]*det(sm)
+		
+	return ans	
+
+
+def cofactor(m):
+
+	cofactors = []
+
+	for i in range(len(m)):
+	
+		temp = []
+	
+		for j in range(len(m)):
+		
+			sm = subMatrix(i,j,m)
+			prod = det(sm)
+			temp.append(pow(-1,i+j)*prod)
+			
+		cofactors.append(temp)
+
+	return cofactors
+		
+n = int(input('Enter the order of the matrix'))
+
+if n <= 0:
+	print('Invalid input')
+	sys.exit()
+	
+m = matrix_create(n)
+
+print('cofactor matrix of given matrix is: ',cofactor(m))
+
+
+
